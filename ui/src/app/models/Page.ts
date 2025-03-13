0a2ce0608e5abb743cd13ad287a98c715834ecac
YASSINE-ABHIR <yassineabhir.info@gmail.com>
2025-03-13 23:19:20 +0000

    first commit 2

--- ui/src/app/models/Page.ts
+++
@@ -1,7 +0,0 @@
-export interface Page<T> {
-  content: T[]; // Data content
-  totalElements: number; // Total elements
-  totalPages: number; // Total pages
-  size: number; // Elements count per page
-  number: number; // Current page index
-}
--- ui/src/environments/environment.development.ts
+++
@@ -1,5 +0,0 @@
-const baseUrl: string = 'http://localhost:8888'
-export const environment = {
-  ragUrl: `${baseUrl}/rag-service`,
-  cryptoUrl: `${baseUrl}/crypto-service`,
-};
